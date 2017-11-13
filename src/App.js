f0a86d11bbc5652d2099b34303e1e5b12fc0073e
Peter Schussheim <peter@schussheim.com>
2017-11-13 17:08:14 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import Spinner from './components/Spinner'
+import spinners from './data/spinners'
+
+const appStyle = {
+  fontFamily: 'Arial',
+  display: 'grid',
+  gridTemplateColumns: '400px',
+  gridColumnGap: '2em',
+  padding: '1.25em 1.25em 1.25em 1.25em'
+}
+
+const wrapperStyle = {
+  border: '1px solid #ddd'
+}
+
+const divStyle = {
+  border: '1px dotted black',
+  margin: '1em',
+  padding: '15px',
+  gridColumnGap: '1em',
+  gridRowGap: '1em',
+  gridTemplateColumns: 'repeat(2,calc(50% - .5em))'
+}
+
+const spinnerStyle = {
+  paddingLeft: '55px',
+  paddingTop: '55px',
+  paddingBottom: '55px',
+  fontFamily: 'Arial'
+}
+
+function createExamples(listOfSpinners) {
+  return Object.keys(listOfSpinners).map((k, i) => (
+    <div style={divStyle} key={i}>
+      {k}: <Spinner style={spinnerStyle} type={k} key={i} />
+    </div>
+  ))
+}
+
+class App extends Component {
+  render() {
+    const elements = createExamples(spinners)
+    return (
+      <div style={appStyle}>
+        <div style={{ margin: '1px' }}>
+          <h1>react-spinners</h1>
+          <h2>examples</h2>
+        </div>
+        <div style={wrapperStyle}>{elements}</div>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/Spinner.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import spinners from '../data/spinners'
+
+class Spinner extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { spinner: this.getSpinnerType(props), frame: 0 }
+    this.getSpinnerType = this.getSpinnerType.bind(this)
+  }
+
+  componentDidMount() {
+    this.intervalId = setInterval(
+      () => this.tick(),
+      this.state.spinner.interval
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
+
+  tick() {
+    this.setState({
+      frame:
+        this.state.frame + 1 === this.state.spinner.frames.length
+          ? 0
+          : this.state.frame + 1
+    })
+  }
+
+  getSpinnerType(props) {
+    const spinnerType = props.type ? props.type : 'dots'
+    const spinner = spinners[spinnerType]
+    return spinner
+  }
+
+  render() {
+    const { spinner, frame } = this.state
+    return <span {...this.props}>{spinner.frames[frame]}&nbsp;</span>
+  }
+}
+
+export default Spinner
